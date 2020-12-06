6e113856aab2883bdea635353cc51573928b6b40
RaviKiranReddyS <27306319+RaviKiranReddyS@users.noreply.github.com>
2020-12-06 10:05:38 -0500

    Added basic UI controls to the recipe and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Hyderabadi Biryani','Spicy & Delicious ','https://c.ndtvimg.com/2018-10/ohqcobr_handi-biryani_625x300_08_October_18.jpg'),
+    new Recipe('South-Indian BreakFast','Breakfast Thali','https://miro.medium.com/max/700/1*InlgltnGNwj7W93dJ_oQ_g.jpeg')
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients : Ingredient[] = [
+    new Ingredient('Basmati Rice Bag',1),
+    new Ingredient('Spices',5),
+    new Ingredient('ID batter',1),
+    new Ingredient('Groundnuts for Chutney',1)
+  ];
   constructor() { }
 
   ngOnInit(): void {
